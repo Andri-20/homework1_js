a52aacecbdeb493154411ac023d7a98d1a6a028a
AndriySobenko <andriysobenko@outlook.com>
2022-07-22 15:58:32 +0300

    js_homework1

---
+++ js.js
@@ -0,0 +1,88 @@
+                      //Task1
+
+let a1='hello';
+let b1='owu';
+let c1='com';
+let d='ua';
+const e=1;
+const f=10;
+const g=-999;
+const h=123;
+let i=3.14;
+let j= 2.7;
+let k=16;
+let bul1=true;
+let bul2=false;
+
+console.log(a1);
+alert(a1);
+document.write(a1+'<br/>');
+
+console.log(b1);
+alert(b1);
+document.write(b1+'<br/>');
+
+console.log(c1);
+alert(c1);
+document.write(c1+'<br/>');
+
+console.log(d);
+alert(d);
+document.write(d+'<br/>');
+
+console.log(e);
+alert(e);
+document.write(e+'<br/>');
+
+console.log(f);
+alert(f);
+document.write(f+'<br/>');
+
+console.log(g);
+alert(g);
+document.write(g+'<br/>');
+
+console.log(h);
+alert(h);
+document.write(h+'<br/>');
+
+console.log(i);
+alert(i);
+document.write(i+'<br/>');
+
+console.log(j);
+alert(j);
+document.write(j+'<br/>');
+
+console.log(k);
+alert(k);
+document.write(k+'<br/>');
+
+console.log(bul1);
+alert(bul1);
+document.write(bul1+'<br/>');
+
+console.log(bul2);
+alert(bul2);
+document.write(bul2+'<br/>');
+   console.log(' ');
+                       //Task2
+let firstName='Andriy';
+let middleName='Bogdanovych';
+let lastName='Sobenko';
+let person=`${lastName} ${firstName} ${middleName}`;
+console.log(person);
+   console.log(' ');
+                       //Task3
+ let a = 100;
+ let b = '100';
+ let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+   console.log(' ');
+                       //Task4
+let firstName2=prompt('Яке твоє ім`я?');
+let middleName2=prompt('По-батькові?');
+let age=prompt('Твій вік?');
+console.log(`${firstName2} ${middleName2} ${age} років`);
